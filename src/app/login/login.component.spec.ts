4008acc079e5edf0ee1a6a93129af4f26a7403f6
Pratiksha Dudhane <pratikshadudhane7378@gmail.com>
2025-05-11 07:09:17 -0700

    Initial commit

---
+++ src/app/login/login.component.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginComponent } from './login.component';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoginComponent ]
+    }).compileComponents();
+  });
+
+  beforeEach((): void => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+//function beforeEach(arg0: "() => Promise<void>)" ,{
+ // throw: , new: Error}:("Function not implemented."));
+
+
+
+
